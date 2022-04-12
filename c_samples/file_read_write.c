83c260d36ca911d9e057376a3f2d451241201461
vivi wu <vivi705@qq.com>
2022-04-12 22:50:34 +0800

    112

--- c_samples/file_read_write.c
+++ c_samples/file_read_write.c
@@ -5,15 +5,37 @@
 #include <stdio.h>
 
 
-void file_write()
-{
-  char str[] = "Created by viviwu on 2022/4/12." ;
-  FILE * of;
-  of = fopen("test.txt", "w");
-  if (of){
-    fprint("test wirte %s\n", str);
-
-    fclose(of);
+void file_write() {
+  FILE *ofp;
+  ofp = fopen("test.txt", "w");
+  if (ofp) {
+    fprintf(ofp, "Created by viviwu on 2022/4/12.\n");          // override
+    fputs("append next line \n", ofp);         // append
+    fclose(ofp);
   }
 
+}
+
+void file_read() {
+  FILE *fp = NULL;
+  char buff[255];
+
+  fp = fopen("test.txt", "r");
+  if (fp) {
+    fscanf(fp, "%s", buff);
+    printf("1: %s\n", buff);
+
+    fgets(buff, 255, (FILE *) fp);
+    printf("2: %s\n", buff);
+
+    fgets(buff, 255, (FILE *) fp);
+    printf("3: %s\n", buff);
+    fclose(fp);
+  }
+
+}
+
+int main() {
+  //file_write();
+  file_read();
 }
